7612160da968c14d379cf7478e84629fa5594eda
refanzafahlevi06-cpu <refanzafahlevi06@gmail.com>
2026-06-05 14:17:53 +0700

    Refanzaa.py

--- Rerfanzaa.py
+++ Rerfanzaa.py
@@ -102,8 +102,7 @@ elif menu == "2. Struktur Model CNN":
         ])
         
         stream = io.StringIO()
-        model.summary(print_fn=lambda x: stream.write(x + '
-'))
+        model.summary(print_fn=lambda x: stream.write(x + ))
         summary_string = stream.getvalue()
         
         st.subheader("Detail Layer Model Keras:")
